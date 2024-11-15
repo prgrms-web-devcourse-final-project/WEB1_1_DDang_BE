5a3bf4766e381c966c897f65d5db0dffcfd9a9c5
Repaion24 <65394501+Repaion24@users.noreply.github.com>
2024-11-15 14:33:14 +0900

    init commit

---
+++ src/main/java/team9/ddang/DDangApplication.java
@@ -0,0 +1,13 @@
+package team9.ddang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DDangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DDangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team9/ddang/DDangApplicationTests.java
@@ -0,0 +1,13 @@
+package team9.ddang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DDangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
