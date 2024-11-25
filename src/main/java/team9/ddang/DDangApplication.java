ef406ed402a30e15d79dd3d46463f3a03f21b87d
rudgns328 <rudgns328@gmail.com>
2024-11-26 00:40:13 +0900

    feat: #6 @EnableJpaAuditing 추가

--- src/main/java/team9/ddang/DDangApplication.java
+++ src/main/java/team9/ddang/DDangApplication.java
@@ -2,7 +2,9 @@ package team9.ddang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DDangApplication {
 
