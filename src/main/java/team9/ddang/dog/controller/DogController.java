6c7f0dc988bd8b6f9f3627ccfbf702c8b23cb37c
highjjjw <highjjw@naver.com>
2024-11-25 11:01:11 +0900

    feat: #4 공통 응답 API 활용

--- src/main/java/team9/ddang/dog/controller/DogController.java
+++ src/main/java/team9/ddang/dog/controller/DogController.java
@@ -1,46 +1,50 @@
 package team9.ddang.dog.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import team9.ddang.dog.controller.request.CreateDogRequest;
 import team9.ddang.dog.controller.request.UpdateDogRequest;
+import team9.ddang.dog.service.request.UpdateDogServiceRequest;
+
+import team9.ddang.dog.dto.GetDogResponse;
 import team9.ddang.dog.service.DogService;
+import team9.ddang.global.api.ApiResponse;
+
+//import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/dogs")
+@RequestMapping("/api/v1/dogs")
 @RequiredArgsConstructor
 public class DogController {
 
     private final DogService dogService;
 
     @PostMapping
-    public ResponseEntity<Void> createDog(@RequestBody CreateDogRequest request) {
-        // Service 계층 호출
+    public ResponseEntity<ApiResponse<Void>> createDog(@RequestBody CreateDogRequest request) {
         dogService.createDog(request.toServiceRequest());
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ResponseEntity.ok(ApiResponse.created(null)); // 데이터가 필요 없으므로 null
     }
 
     @GetMapping("/{id}")
-    public <GetDogResponse> ResponseEntity<GetDogResponse> getDogById(@PathVariable Long id) {
-        GetDogResponse response = (GetDogResponse) dogService.getDogById(id);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<ApiResponse<GetDogResponse>> getDogById(@PathVariable Long id) {
+        GetDogResponse response = dogService.getDogById(id);
+        return ResponseEntity.ok(ApiResponse.ok(response));
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<Void> updateDog(
+    public ResponseEntity<ApiResponse<Void>> updateDog(
             @PathVariable Long id,
             @RequestBody UpdateDogRequest request
     ) {
         dogService.updateDog(request.toServiceRequest(id));
-        return ResponseEntity.noContent().build(); // 업데이트 성공 시 204 반환
+        return ResponseEntity.ok(ApiResponse.ok(null)); // 성공 시 메시지만 반환
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteDog(@PathVariable Long id) {
+    public ResponseEntity<ApiResponse<Void>> deleteDog(@PathVariable Long id) {
         dogService.deleteDog(id);
-        return ResponseEntity.noContent().build(); // 삭제 성공 시 204 반환
+        return ResponseEntity.ok(ApiResponse.ok(null)); // 성공 메시지만 반환
     }
-
 }
+
