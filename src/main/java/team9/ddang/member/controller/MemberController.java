19ad38706ac5bcb8cb779e26fdfe615993ac4f9f
rudgns328 <rudgns328@gmail.com>
2024-11-26 00:36:44 +0900

    feat: #6 MemberController 토큰 재발급, 로그아웃 기능 추가, Swagger 적용

--- src/main/java/team9/ddang/member/controller/MemberController.java
+++ src/main/java/team9/ddang/member/controller/MemberController.java
@@ -1,69 +1,176 @@
 package team9.ddang.member.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 import team9.ddang.global.api.ApiResponse;
 import team9.ddang.member.controller.request.JoinRequest;
-import team9.ddang.member.entity.Provider;
-import team9.ddang.member.oauth2.CustomOAuth2User;
-import team9.ddang.member.repository.MemberRepository;
 import team9.ddang.member.service.MemberService;
 import team9.ddang.member.service.response.MemberResponse;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/member")
+@Tag(name = "Member API", description = "멤버 관련 API")
 @Slf4j
 public class MemberController {
 
-    private final MemberRepository memberRepository;
     private final MemberService memberService;
-
-    @GetMapping("/sign-up")
-    @PreAuthorize("hasRole('ROLE_GUEST')")
-    public ApiResponse<String> signUpPage() {
-        // ROLE_GUEST 사용자만 접근 가능
-        return ApiResponse.ok("추가 정보를 입력해주세요.");
-    }
-
-//    @PostMapping("/join")
-//    @PreAuthorize("hasRole('ROLE_GUEST')")
-//    public ApiResponse<MemberResponse> join(@RequestBody @Valid JoinRequest request,
-//                                            @AuthenticationPrincipal OAuth2User principal,
-//                                            HttpServletResponse response) {
-//
-//        CustomOAuth2User customOAuth2User = (CustomOAuth2User) principal;
-//        Provider provider = customOAuth2User.getMember().getProvider();
-//        String email = customOAuth2User.getMember().getEmail();
-//
-//        return ApiResponse.created(memberService.join(request.toServiceRequest(email, provider), response));
-//    }
 
     @PostMapping("/join")
     @PreAuthorize("hasRole('ROLE_GUEST')")
-    public ApiResponse<MemberResponse> join(@RequestBody @Valid JoinRequest request,
-                                            @AuthenticationPrincipal CustomOAuth2User customOAuth2User,
+    @Operation(
+            summary = "회원가입",
+            description = "OAuth2 로그인 후 /register추가 정보 기입 후 회원가입을 완료합니다.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "회원가입 정보",
+                    required = true,
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = JoinRequest.class)
+                    )
+    ),
+            responses = {
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                            responseCode = "200",
+                            description = "회원가입 성공",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = MemberResponse.class)
+                            )
+                    ),
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                            responseCode = "400",
+                            description = "회원가입 실패",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ApiResponse.class)
+                            )
+                    ),
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                            responseCode = "401",
+                            description = "인증 실패",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ApiResponse.class)
+                            )
+                    ),
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                            responseCode = "500",
+                            description = "서버 오류",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ApiResponse.class)
+                            )
+                    )
+            }
+    )
+    public ApiResponse<MemberResponse> join(@RequestBody @Valid JoinRequest joinRequest,
                                             HttpServletResponse response) {
 
-        log.info("SecurityContextHolder 인증 정보: {}", SecurityContextHolder.getContext().getAuthentication());
-        log.info("CustomOAuth2User: {}", customOAuth2User);
-
-        if (customOAuth2User == null || customOAuth2User.getEmail() == null) {
-            throw new IllegalArgumentException("인증된 사용자가 없습니다.");
-        }
-
-        String email = customOAuth2User.getEmail(); // OAuth2에서 제공된 이메일
-        Provider provider = customOAuth2User.getProvider(); // OAuth2 Provider 정보
-
-        // 회원가입 서비스 호출
-        return ApiResponse.created(memberService.join(request.toServiceRequest(email, provider), response));
+        return ApiResponse.created(memberService.join(joinRequest.toServiceRequest(), response));
     }
 
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @PostMapping("/reissue")
+    @Operation(
+            summary = "AccessToken 재발급",
+            description = "RefreshToken을 사용하여 새로운 AccessToken을 발급합니다.",
+            responses = {
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                            responseCode = "200",
+                            description = "AccessToken 재발급 성공",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ApiResponse.class)
+                            )
+                    ),
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                            responseCode = "400",
+                            description = "AccessToken 재발급 실패",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ApiResponse.class)
+                            )
+                    ),
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                            responseCode = "401",
+                            description = "인증 실패",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ApiResponse.class)
+                            )
+                    ),
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                            responseCode = "500",
+                            description = "서버 오류",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ApiResponse.class)
+                            )
+                    )
+            }
+    )
+    public ApiResponse<String> reissue(HttpServletRequest request, HttpServletResponse response) {
+        log.info("reissue() 메서드 진입");
+
+        String newAccessToken = memberService.reissueAccessToken(request, response);
+        log.info("새로운 AccessToken 생성: {}", newAccessToken);
+
+        // ApiResponse 사용하여 응답 반환
+        return ApiResponse.ok(newAccessToken);
+    }
+
+    @DeleteMapping("/logout")
+    @Operation(
+            summary = "로그아웃",
+            description = "로그아웃을 수행합니다.",
+            responses = {
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                            responseCode = "200",
+                            description = "로그아웃 성공",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ApiResponse.class)
+                            )
+                    ),
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                            responseCode = "400",
+                            description = "로그아웃 실패",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ApiResponse.class)
+                            )
+                    ),
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                            responseCode = "401",
+                            description = "인증 실패",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ApiResponse.class)
+                            )
+                    ),
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                            responseCode = "500",
+                            description = "서버 오류",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ApiResponse.class)
+                            )
+                    )
+            }
+    )
+    public ApiResponse<String> logout(HttpServletRequest request) {
+        log.info("logout() 메서드 진입");
+        return ApiResponse.ok(memberService.logout(request));
+    }
 }
+
