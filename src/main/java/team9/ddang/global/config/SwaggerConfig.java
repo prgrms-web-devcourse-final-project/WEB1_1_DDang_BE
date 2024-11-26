6cfe7ee160cfae78b86b1081b42e5f06a4c147e3
rudgns328 <rudgns328@gmail.com>
2024-11-26 10:52:12 +0900

    feat: #6 토큰 발급을 위한 SwaggerConfig 작성

---
+++ src/main/java/team9/ddang/global/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package team9.ddang.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("DDang API 문서")
+                .description("잘못된 부분이나 오류 발생 시 말씀해주세요.");
+
+        SecurityScheme bearerAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        SecurityRequirement addSecurityItem = new SecurityRequirement();
+        addSecurityItem.addList("Bearer Token");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("Bearer Token", bearerAuth))
+                .addSecurityItem(addSecurityItem)
+                .info(info);
+    }
+}
+
