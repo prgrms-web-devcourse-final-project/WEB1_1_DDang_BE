b7edf97e1fbf96545317c373cca6a9ade64cba71
rudgns328 <rudgns328@gmail.com>
2024-11-29 11:56:42 +0900

    fix: #51 잘못 선언된 import 문 제거

--- src/main/java/team9/ddang/dog/repository/MemberDogRepository.java
+++ src/main/java/team9/ddang/dog/repository/MemberDogRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import team9.ddang.dog.entity.MemberDog;
 import team9.ddang.member.entity.Member;
 import team9.ddang.walk.service.response.walk.MemberNearbyInfo;
