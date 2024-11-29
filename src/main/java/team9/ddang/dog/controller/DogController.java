f9bc9aae0c46bd53567a0473739455ccb0375ac0
rudgns328 <rudgns328@gmail.com>
2024-11-29 15:44:23 +0900

    feat: #54 Dog Swagger 작성

--- src/main/java/team9/ddang/dog/controller/DogController.java
+++ src/main/java/team9/ddang/dog/controller/DogController.java
@@ -1,5 +1,7 @@
 package team9.ddang.dog.controller;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -23,7 +25,19 @@ public class DogController {
 
     private final DogService dogService;
 
-    @PostMapping
+    @PostMapping("/create")
+    @Operation(
+            summary = "반려견 등록",
+            description = "반려견을 등록합니다.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "반려견 등록 정보",
+                    required = true,
+                    content = @io.swagger.v3.oas.annotations.media.Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = CreateDogRequest.class)
+                    )
+            )
+    )
     public ApiResponse<CreateDogResponse> createDog(
             @RequestBody @Valid CreateDogRequest request,
             @AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
@@ -33,6 +47,10 @@ public class DogController {
     }
 
     @GetMapping("/{id}")
+    @Operation(
+            summary = "반려견 조회",
+            description = "반려견을 조회합니다."
+    )
     public ApiResponse<GetDogResponse> getMyDog(
             @AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
 
@@ -47,6 +65,18 @@ public class DogController {
     }
 
     @PatchMapping("/{id}")
+    @Operation(
+            summary = "반려견 정보 수정",
+            description = "반려견 정보를 수정합니다.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "반려견 수정 정보",
+                    required = true,
+                    content = @io.swagger.v3.oas.annotations.media.Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = UpdateDogRequest.class)
+                    )
+            )
+    )
     public ApiResponse<Void> updateDog(
             @PathVariable Long id,
             @RequestBody @Valid UpdateDogRequest request,
@@ -58,6 +88,10 @@ public class DogController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(
+            summary = "반려견 삭제",
+            description = "반려견을 삭제합니다."
+    )
     public ApiResponse<Void> deleteDog(
             @PathVariable Long id,
             @AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
