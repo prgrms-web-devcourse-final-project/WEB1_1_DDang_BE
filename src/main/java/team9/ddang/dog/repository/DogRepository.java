57ee40c1901660ad27728e5029e98432f775d340
rudgns328 <rudgns328@gmail.com>
2024-11-29 11:56:33 +0900

    fix: #51 잘못 선언된 import 문 제거

--- src/main/java/team9/ddang/dog/repository/DogRepository.java
+++ src/main/java/team9/ddang/dog/repository/DogRepository.java
@@ -4,7 +4,6 @@ import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import team9.ddang.dog.entity.Dog;
 import team9.ddang.family.entity.Family;
 
