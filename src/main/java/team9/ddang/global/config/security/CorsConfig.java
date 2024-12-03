7c47c3ad59fc60a7421218a7b975552e8b3b4ffc
Repaion24 <65394501+Repaion24@users.noreply.github.com>
2024-12-03 10:16:07 +0900

    fix: #66 모든 경로 cors 설정 적용

--- src/main/java/team9/ddang/global/config/security/CorsConfig.java
+++ src/main/java/team9/ddang/global/config/security/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
         config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/api/**", config);
+        source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
 }
