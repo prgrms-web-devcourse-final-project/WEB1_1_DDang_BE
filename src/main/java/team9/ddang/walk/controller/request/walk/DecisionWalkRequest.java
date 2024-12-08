46258e81f8a90657a163e69ea63e6735a6eba709
MoonJaeGyeong <bblbblan69@gmail.com>
2024-12-08 14:49:53 +0900

    docs : #103 산책 웹소켓 에러 응답 명세 추가

--- src/main/java/team9/ddang/walk/controller/request/walk/DecisionWalkRequest.java
+++ src/main/java/team9/ddang/walk/controller/request/walk/DecisionWalkRequest.java
@@ -1,11 +1,13 @@
 package team9.ddang.walk.controller.request.walk;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import team9.ddang.walk.service.request.walk.DecisionWalkServiceRequest;
 
 public record DecisionWalkRequest(
-        @NotNull(message = "상대방 이메일은 필수 입력 사항입니다.")
+        @NotNull(message = "상대 이메일은 입력해주셔야 해요")
+        @Email(message = "올바른 이메일 형식을 입력해주세요.")
         @Schema(description = "상대방 이메일", example = "example@exaple.com")
         String otherEmail,
         @Schema(description = "수락 거절 여부", example = "ACCEPT")
