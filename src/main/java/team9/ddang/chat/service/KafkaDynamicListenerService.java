7cfcdc90fc0ac3c0b4a1cfbdad4f448d16c465f7
Repaion24 <65394501+Repaion24@users.noreply.github.com>
2024-12-09 03:36:46 +0900

    feat: #99 KafkaService 통합

--- src/main/java/team9/ddang/chat/service/KafkaDynamicListenerService.java
+++ src/main/java/team9/ddang/chat/service/KafkaDynamicListenerService.java
@@ -1,57 +1,59 @@
-package team9.ddang.chat.service;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
-import org.springframework.kafka.listener.MessageListener;
-import org.springframework.stereotype.Service;
-import team9.ddang.chat.consumer.ChatConsumer;
-
-import java.util.HashMap;
-import java.util.Map;
-
-@Service
-@RequiredArgsConstructor
-@Slf4j
-public class KafkaDynamicListenerService {
-
-    private final ConcurrentKafkaListenerContainerFactory<String, String> factory;
-    private final Map<String, ConcurrentMessageListenerContainer<String, String>> containers = new HashMap<>();
-    private final ChatConsumer chatConsumer;
-
-    public void addListenerForChatRoom(Long chatRoomId) {
-        String topicName = "topic-chat-" + chatRoomId;
-
-        if (containers.containsKey(topicName)) {
-            log.info("Listener for topic '{}' already exists.", topicName);
-            return;
-        }
-
-        ConcurrentMessageListenerContainer<String, String> container = factory.createContainer(topicName);
-        container.getContainerProperties().setMessageListener((MessageListener<String, String>) record -> {
-            log.info("Received message for topic '{}': {}", topicName, record.value());
-
-            if (record.value().contains("\"readMessageIds\"")) {
-                chatConsumer.consumeReadEvent(topicName, record.value());
-            } else {
-                chatConsumer.consumeMessage(topicName, record.value());
-            }
-        });
-
-        container.start();
-        containers.put(topicName, container);
-
-        log.info("Started Kafka listener for topic '{}'", topicName);
-    }
-
-    public void removeListenerForChatRoom(Long chatRoomId) {
-        String topicName = "topic-chat-" + chatRoomId;
-
-        ConcurrentMessageListenerContainer<String, String> container = containers.remove(topicName);
-        if (container != null) {
-            container.stop();
-            log.info("Stopped Kafka listener for topic '{}'", topicName);
-        }
-    }
-}
+//package team9.ddang.chat.service;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+//import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+//import org.springframework.kafka.listener.MessageListener;
+//import org.springframework.stereotype.Service;
+//import team9.ddang.chat.consumer.ChatConsumer;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Service
+//@RequiredArgsConstructor
+//@Slf4j
+//public class KafkaDynamicListenerService {
+//
+//    private final ConcurrentKafkaListenerContainerFactory<String, String> factory;
+//    private final Map<String, ConcurrentMessageListenerContainer<String, String>> containers = new HashMap<>();
+//    private final ChatConsumer chatConsumer;
+//    /**
+//     * 동적 Kafka Listener: "topic-chat-{chatroomId}"을 구독 및 처리
+//     */
+//    public void addListenerForChatRoom(Long chatRoomId) {
+//        String topicName = "topic-chat-" + chatRoomId;
+//
+//        if (containers.containsKey(topicName)) {
+//            log.info("Listener for topic '{}' already exists.", topicName);
+//            return;
+//        }
+//
+//        ConcurrentMessageListenerContainer<String, String> container = factory.createContainer(topicName);
+//        container.getContainerProperties().setMessageListener((MessageListener<String, String>) record -> {
+//            log.info("Received message for topic '{}': {}", topicName, record.value());
+//
+//            if (record.value().contains("\"readMessageIds\"")) {
+//                chatConsumer.consumeReadEvent(topicName, record.value());
+//            } else {
+//                chatConsumer.consumeMessage(topicName, record.value());
+//            }
+//        });
+//
+//        container.start();
+//        containers.put(topicName, container);
+//
+//        log.info("Started Kafka listener for topic '{}'", topicName);
+//    }
+//
+//    public void removeListenerForChatRoom(Long chatRoomId) {
+//        String topicName = "topic-chat-" + chatRoomId;
+//
+//        ConcurrentMessageListenerContainer<String, String> container = containers.remove(topicName);
+//        if (container != null) {
+//            container.stop();
+//            log.info("Stopped Kafka listener for topic '{}'", topicName);
+//        }
+//    }
+//}
