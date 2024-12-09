78c7acc3f2df3c78c794d02a8b2c88ab6323bd7e
Repaion24 <65394501+Repaion24@users.noreply.github.com>
2024-12-09 17:27:21 +0900

    feat: #99 응답 형식 수정

--- src/main/java/team9/ddang/family/service/response/WalkScheduleInfo.java
+++ src/main/java/team9/ddang/family/service/response/WalkScheduleInfo.java
@@ -2,8 +2,11 @@ package team9.ddang.family.service.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import team9.ddang.family.entity.DayOfWeek;
+import team9.ddang.member.entity.Member;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public record WalkScheduleInfo(
         @Schema(description = "산책 일정 ID", example = "1")
@@ -13,6 +16,13 @@ public record WalkScheduleInfo(
         DayOfWeek dayOfWeek,
 
         @Schema(description = "산책 시간", example = "10:00")
-        LocalTime walkTime
+        String walkTime
 ) {
+        public WalkScheduleInfo(Long walkScheduleId, DayOfWeek dayOfWeek, LocalTime walkTime) {
+                this(
+                        walkScheduleId,
+                        dayOfWeek,
+                        walkTime.format(DateTimeFormatter.ofPattern("HH:mm"))
+                );
+        }
 }
